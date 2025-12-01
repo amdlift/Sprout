779daebf4706d85726c6ad6f7adb13c9094d7ec1
Exo3 <Exo3@10.1.126.77>
2025-12-01 10:38:46 -0600

    added jar

--- dist/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sprout"},{"l":"sprout.controller"},{"l":"sprout.model"},{"l":"sprout.view"}];updateSearchResults();
--- dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"sprout.model","l":"Persistence"},{"p":"sprout","l":"Sprout"},{"p":"sprout.controller","l":"SproutController"},{"p":"sprout.view","l":"SproutView"},{"p":"sprout.model","l":"TodoItem"},{"p":"sprout.view","l":"TreeGraphics"}];updateSearchResults();
